49ae574e0179436195836e8e2a2808e27c5b6a1b
evilbob27 <evilbob27@ymail.com>
2024-06-19 12:15:58 -0700

    completed UI rough draft

---
+++ src/mainsection.jsx
@@ -0,0 +1,14 @@
+export default function MainSection() {
+    return (
+        <>
+            <section className="main-section">
+                <img className="card" src="/tabernacle-ph.png"></img>
+                <input className="search-bar" type="search"></input>
+                <div className="player-section">
+                <img className="profile-picture" src="/sarkhan-ph.png"></img>
+                <p className="life-counter">20</p>
+                </div>
+            </section>
+        </>
+    )
+}
