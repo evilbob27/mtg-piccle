ddad7dcf0e2dcc45582e4f54fdc6a0fc69184753
evilbob27 <evilbob27@ymail.com>
2024-09-09 20:56:04 -0700

    updated grid size to 24, added randomized fade feature

--- src/mainsection.jsx
+++ src/mainsection.jsx
@@ -4,7 +4,8 @@ export default function MainSection() {
     const [life, setLife] = useState(20);
     const [showLostGameModal, setShowLostGameModal] = useState(false);
     const [grid, setGrid] = useState([]);
-    
+    const [hiddenSquares, setHiddenSquares] = useState([]);
+
     const guessCard = () => {
         setLife(life => {
             const newLife = life - 4;
@@ -14,42 +15,53 @@ export default function MainSection() {
             }
             return newLife;
         });
+
+        const newHiddenSquares = [...hiddenSquares];
+        for (let i = 0; i < 4; i++) {
+            let randomIndex;
+            do {
+                randomIndex = Math.floor(Math.random() * 24);
+            } while (newHiddenSquares.includes(randomIndex));
+            newHiddenSquares.push(randomIndex);
+        }
+
+        setHiddenSquares(newHiddenSquares);
     };
 
-    useEffect(() => {
-        const generateGrid = () => {
-            const newGrid = [];
-            for (let i = 0; i < 20; i++) {
-                newGrid.push(<div key={i} className="square overlay-square"></div>);
-            }
-            setGrid(newGrid);
-        };
+useEffect(() => {
+    const generateGrid = () => {
+        const newGrid = [];
+        for (let i = 0; i < 24; i++) {
+            newGrid.push(<div key={i} className={`square overlay-square ${hiddenSquares.includes(i) ? `active` : ``}`}></div>);
+        }
+        setGrid(newGrid);
+    };
 
-        generateGrid();
-    }, []);
+    generateGrid();
+}, [hiddenSquares]);
 
-    return (
-        <section className="main-section">
-			<article className="card-area">
-				<img className="card" src="/images/tabernacle-ph.png"></img>
-				<div className="hidden-area">
-					<div className="square"></div>
-					<div className="square"></div>
-					<div className="square"></div>
-					<div className="square"></div>
-                    {grid}
-				</div>
-			</article>
-            <input className="search-bar" type="search" placeholder="Search Card..."></input>
-            <div className="player-section">
-                <img className="profile-picture" src="/images/sarkhan-ph.png"></img>
-                <p className="life-counter">{life}</p>
+return (
+    <section className="main-section">
+        <article className="card-area">
+            <img className="card" src="/images/tabernacle-ph.png"></img>
+            <div className="hidden-area">
+                <div className="square"></div>
+                <div className="square"></div>
+                <div className="square"></div>
+                <div className="square"></div>
+                {grid}
             </div>
-            {
-                showLostGameModal && (<p className="game-over">DEFEAT</p>)
-            }
+        </article>
+        <input className="search-bar" type="search" placeholder="Search Card..."></input>
+        <div className="player-section">
+            <img className="profile-picture" src="/images/sarkhan-ph.png"></img>
+            <p className="life-counter">{life}</p>
+        </div>
+        {
+            showLostGameModal && (<p className="game-over">DEFEAT</p>)
+        }
 
-            <button className="life-test" onClick={guessCard}>Guess</button>
-        </section>
-    );
-}
+        <button className="life-test" onClick={guessCard}>Guess</button>
+    </section>
+);
+};
